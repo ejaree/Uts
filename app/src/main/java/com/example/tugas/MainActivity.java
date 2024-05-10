56d307f22128aa5a018f7d061058e91e8b7ad031
ejaree <getmorelicks@gmail.com>
2024-05-10 13:11:50 +0700

    UTS First Project

---
+++ app/src/main/java/com/example/tugas/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.tugas;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initToolbar();
+        initComponent();
+    }
+
+    private void initToolbar() {
+        Tools.setSystemBarColor(this, R.color.grey_5);
+        Tools.setSystemBarLight(this);
+    }
+
+    private void initComponent() {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
